85b882fca4752b3a7567d596a57ff9b2453865c8
LeoZhang <balabagi2012@gmail.com>
2024-05-14 23:34:21 +0800

    feat: UI changes

--- src/app/[userId]/page.tsx
+++ src/app/[userId]/page.tsx
@@ -1,8 +1,10 @@
 import Profile from "@/components/Profile";
+import { Typography } from "@mui/material";
 
 const UserPage = ({ params }: { params: { userId: string } }) => {
   return (
     <main>
+      <Typography variant="h4">User Profile Page</Typography>
       <Profile userId={params.userId} />
     </main>
   );
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,8 +1,10 @@
 import AuthForm from "@/components/AuthForm";
+import { Typography } from "@mui/material";
 
 const AuthPage = async () => {
   return (
     <main>
+      <Typography variant="h4">User System Page</Typography>
       <AuthForm />
     </main>
   );
