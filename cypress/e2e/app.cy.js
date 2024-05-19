a86b819d37369138e2bef64449224241db82b3bd
LeoZhang <balabagi2012@gmail.com>
2024-05-20 00:59:37 +0800

    feat: navigation for no auth e2e testing

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -13,5 +13,17 @@ describe('Navigation', () => {
         cy.get('h4').should("contain", 'User System')
     })
 
+    it('should navigate to the auth page when no auth', () => {
+        // Start from the index page
+        cy.visit('http://localhost:3000/auth/emailVerification')
 
+        // Wait for auto redirect
+        cy.wait(2000)
+
+        // The new url should include "/about"
+        cy.url().should('include', '/auth')
+
+        // The new page should contain an h4 with "User System"
+        cy.get('h4').should("contain", 'User System')
+    })
 })
